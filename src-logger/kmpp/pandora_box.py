5a2a579915fb2f4aaaf73d1ec590cb96d89a4d52
mocatfrio <hafarafirdausi@gmail.com>
2019-04-28 18:05:26 +0700

    add src with logger

--- src-logger/kmpp/pandora_box.py
+++ src-logger/kmpp/pandora_box.py
@@ -1,4 +1,5 @@
 import csv
+from .custom_logger import logger
 
 class PandoraBox:
   def __init__(self, total_prod, max_ts):
@@ -11,17 +12,26 @@ class PandoraBox:
       except:
         last_ts = 0
       finally:
+        logger.info('Last update 1 pbox[{}] = {}'.format(id, last_ts))
         if last_ts > 0:
           self.update_score(id, last_ts, timestamp, dsl[id]['last_prob'])
         if last_ts != timestamp:
           self.box[id][timestamp] += score
+          logger.info('Add score pbox[{}][{}] + {} = {}'. format(id, timestamp, score, self.box[id][timestamp]))
 
   def update_score(self, id, last_ts, now_ts, prob):
     for i in range(last_ts + 1, now_ts):
       self.box[id][i] += prob
+      logger.info('Update score pbox[{}][{}] + {} = {}'.format(id, i, prob, self.box[id][i]))
+
+  def print_box(self):
+    logger.info('Pandora Box')
+    for row in self.box:
+      logger.info(row)
 
   def export_csv(self):
     with open('../dataset/pandora_box.csv', 'w') as csvFile:
       writer = csv.writer(csvFile)
       writer.writerows(self.box)
     csvFile.close()
+    logger.info('pandora box csv exported succesfully')
--- src-logger/precompute.py
+++ src-logger/precompute.py
@@ -2,12 +2,14 @@ import csv
 from kmpp.customer_thread import CustThread
 from kmpp.pandora_box import PandoraBox
 from kmpp.event_queue import EventQueue
+from kmpp.custom_logger import logger
 
 def product_in(prod_id, threads, prod_active, timestamp, act, last_event):
   try:
     wait_thread_event(threads, last_event)
   finally:
     prod_active.append(prod_id)    
+    logger.info('[P-{} in] Masuk ke produk aktif: {}'.format(prod_id, prod_active))  
     for thread in threads.values():
       thread.notify(timestamp, prod_id, act)
 
@@ -18,11 +20,13 @@ def product_out(prod_id, threads, prod_active, timestamp, act, last_event):
     for thread in threads.values():
       thread.notify(timestamp, prod_id, act, prod_active.copy())
     prod_active.remove(prod_id)
+    logger.info('[P-{} out] Hapus dari produk aktif: {}'.format(prod_id, prod_active))
 
 def customer_in(cust_id, threads, prod_active, timestamp, last_event):
   try:
     wait_thread_event(threads, last_event)  
   finally:
+    logger.info('[C-{} in] Make thread'.format(cust_id))
     threads[cust_id] = CustThread(int(cust_id), timestamp, prod_active.copy())
     threads[cust_id].start()
 
@@ -30,10 +34,12 @@ def customer_out(cust_id, threads, timestamp, last_event):
   while True:
     if threads[cust_id].get_last_event() == last_event:
       break
+  logger.info('[C-{} out] Kill thread'.format(cust_id))
   threads[cust_id].kill_thread(timestamp)
 
 def input_csv(name, file, event_queue):
   data = {}
+  logger.info('Input ({}) data... in {}'.format(name, file))
   with open(file, 'r') as csv_file:
     csv_reader = csv.DictReader(csv_file, delimiter=',')
     for row in csv_reader:
@@ -47,7 +53,14 @@ def input_csv(name, file, event_queue):
         data[id]['value'].append(int(row[csv_reader.fieldnames[i]]))
       for i in range(0, 2):
         event_queue.enqueue(data[id]['ts_in'] if i == 0 else data[id]['ts_out'], 0 if name == 'product' else 1, id, i)
+  logger.info('Data ({}) succesfully inputed'.format(name))
   return data
+
+# def print_data(data):
+#   for key, values in data.items():
+    # logger.info('{}'.format(key))
+#     for k, v in values.items():
+      # logger.info('{} : {}'.format(k, v))
 
 def insert_thread_data(data, pandora_box):
   CustThread.data = data
@@ -67,6 +80,7 @@ if __name__ == "__main__":
   dataset = ['../dataset/product.csv', '../dataset/customer.csv']
   data['product'] = input_csv('product', dataset[0], event_queue)
   data['customer'] = input_csv('customer', dataset[1], event_queue)
+  # print_data(data)
   
   event_queue.sort_queue()
   pandora_box = PandoraBox(len(data['product']) + 1, event_queue.get_max_timestamp() + 1)
@@ -93,4 +107,7 @@ if __name__ == "__main__":
   for thread in threads.values():
     thread.join()
 
+  pandora_box.print_box()
   pandora_box.export_csv()
+
+  logger.info('Exiting the main program') 
