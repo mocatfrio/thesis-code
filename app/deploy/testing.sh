61ad1e60bea3f2d14a60ccd4063305d6aa2a6fed
mocatfrio <hafarafirdausi@gmail.com>
2019-06-22 03:37:59 +0700

    deploy testing

--- app/deploy/testing.sh
+++ app/deploy/testing.sh
@@ -1,6 +1,26 @@
-# coba doang
-# python3 precompute.py 'dataset/ind/product_i_500_2.csv' 'dataset/ind/customer_i_500_2.csv' 1
-# python3 precompute.py 'dataset/ind/product_i_500_2.csv' 'dataset/ind/customer_i_500_2.csv' 2
-# python3 precompute.py 'dataset/ant/product_ac_500_2.csv' 'dataset/ant/customer_ac_500_2.csv' 1
-# python3 precompute.py 'dataset/ant/product_ac_500_2.csv' 'dataset/ant/customer_ac_500_2.csv' 2
-python3 precompute.py 'dataset/ant/product_ac_10000_3.csv' 'dataset/ant/customer_ac_10000_3.csv' 3
+#!/bin/bash
+
+python3 precompute.py 'dataset/ind/product_i_10000_3.csv' 'dataset/ind/customer_i_10000_3.csv' 1
+python3 precompute.py 'dataset/ind/product_i_10000_3.csv' 'dataset/ind/customer_i_10000_3.csv' 2
+python3 precompute.py 'dataset/ind/product_i_10000_3.csv' 'dataset/ind/customer_i_10000_3.csv' 3
+printf "skenario 1 done\n"
+
+python3 precompute.py 'dataset/ind/product_i_20000_3.csv' 'dataset/ind/customer_i_20000_3.csv' 1
+python3 precompute.py 'dataset/ind/product_i_20000_3.csv' 'dataset/ind/customer_i_20000_3.csv' 2
+python3 precompute.py 'dataset/ind/product_i_20000_3.csv' 'dataset/ind/customer_i_20000_3.csv' 3
+printf "skenario 2 done\n"
+
+python3 precompute.py 'dataset/ind/product_i_50000_3.csv' 'dataset/ind/customer_i_50000_3.csv' 1
+python3 precompute.py 'dataset/ind/product_i_50000_3.csv' 'dataset/ind/customer_i_50000_3.csv' 2
+python3 precompute.py 'dataset/ind/product_i_50000_3.csv' 'dataset/ind/customer_i_50000_3.csv' 3
+printf "skenario 3 done\n"
+
+python3 precompute.py 'dataset/ind/product_i_100000_3.csv' 'dataset/ind/customer_i_100000_3.csv' 1
+python3 precompute.py 'dataset/ind/product_i_100000_3.csv' 'dataset/ind/customer_i_100000_3.csv' 2
+python3 precompute.py 'dataset/ind/product_i_100000_3.csv' 'dataset/ind/customer_i_100000_3.csv' 3
+printf "skenario 4 done\n"
+
+python3 precompute.py 'dataset/ind/product_i_20000_3.csv' 'dataset/ind/customer_i_20000_3.csv' 1
+python3 precompute.py 'dataset/ind/product_i_20000_3.csv' 'dataset/ind/customer_i_20000_3.csv' 2
+python3 precompute.py 'dataset/ind/product_i_20000_3.csv' 'dataset/ind/customer_i_20000_3.csv' 3
+printf "skenario 5 done\n"
