902a7d7ef36b30834d637ed35231cb5d3a7bb165
mocatfrio <hafarafirdausi@gmail.com>
2019-06-22 10:43:07 +0700

    script clear cache

--- app/dataset-generator/generate_independent_dataset.sh
+++ app/dataset-generator/generate_independent_dataset.sh
@@ -5,9 +5,5 @@
 #   rm -rf ${MY_DIR}*
 # fi
 
-python3 generate_dataset.py i 2000 3 product
-python3 generate_dataset.py i 2000 3 customer
-python3 generate_dataset.py i 3000 3 product
-python3 generate_dataset.py i 3000 3 customer
-python3 generate_dataset.py i 4000 3 product
-python3 generate_dataset.py i 4000 3 customer
+python3 generate_dataset.py i 20000 3 product
+python3 generate_dataset.py i 20000 3 customer
