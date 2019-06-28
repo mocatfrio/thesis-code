db59601090da3207eda267cbc36a51e12c68b873
mocatfrio <hafarafirdausi@gmail.com>
2019-06-29 06:28:46 +0700

    testing new

--- app/dataset-generator/generate_anti_correlated_dataset.sh
+++ app/dataset-generator/generate_anti_correlated_dataset.sh
@@ -6,31 +6,29 @@
 # fi
 
 # skenario 1
-python3 generate_dataset.py ac 10000 3 product
-python3 generate_dataset.py ac 10000 3 customer
-python3 generate_dataset.py ac 20000 3 product
-python3 generate_dataset.py ac 20000 3 customer
-python3 generate_dataset.py ac 50000 3 product
-python3 generate_dataset.py ac 50000 3 customer
-python3 generate_dataset.py ac 100000 3 product
-python3 generate_dataset.py ac 100000 3 customer
-python3 generate_dataset.py ac 200000 3 product
-python3 generate_dataset.py ac 200000 3 customer
+# python3 generate_dataset.py ac 10000 3 product
+# python3 generate_dataset.py ac 10000 3 customer
+# python3 generate_dataset.py ac 20000 3 product
+# python3 generate_dataset.py ac 20000 3 customer
+# python3 generate_dataset.py ac 50000 3 product
+# python3 generate_dataset.py ac 50000 3 customer
+# python3 generate_dataset.py ac 100000 3 product
+# python3 generate_dataset.py ac 100000 3 customer
+# python3 generate_dataset.py ac 200000 3 product
+# python3 generate_dataset.py ac 200000 3 customer
 
-# skenario 2
-python3 generate_dataset.py ac 10000 4 product
-python3 generate_dataset.py ac 10000 4 customer
-python3 generate_dataset.py ac 10000 5 product
-python3 generate_dataset.py ac 10000 5 customer
-python3 generate_dataset.py ac 10000 6 product
-python3 generate_dataset.py ac 10000 6 customer
-python3 generate_dataset.py ac 10000 7 product
-python3 generate_dataset.py ac 10000 7 customer
+# # skenario 2
+# python3 generate_dataset.py ac 10000 4 product
+# python3 generate_dataset.py ac 10000 4 customer
+# python3 generate_dataset.py ac 10000 5 product
+# python3 generate_dataset.py ac 10000 5 customer
+# python3 generate_dataset.py ac 10000 6 product
+# python3 generate_dataset.py ac 10000 6 customer
+# python3 generate_dataset.py ac 10000 7 product
+# python3 generate_dataset.py ac 10000 7 customer
 
 # coba doang
-python3 generate_dataset.py ac 500 2 product
-python3 generate_dataset.py ac 500 2 customer
-python3 generate_dataset.py ac 1000 2 product
-python3 generate_dataset.py ac 1000 2 customer
-python3 generate_dataset.py ac 2000 2 product
-python3 generate_dataset.py ac 2000 2 customer
+python3 generate_dataset.py ac 2000 3 product
+python3 generate_dataset.py ac 2000 3 customer
+python3 generate_dataset.py ac 5000 3 product
+python3 generate_dataset.py ac 5000 3 customer
--- app/dataset-generator/generate_independent_dataset.sh
+++ app/dataset-generator/generate_independent_dataset.sh
@@ -5,32 +5,37 @@
 #   rm -rf ${MY_DIR}*
 # fi
 
-# skenario 1
-python3 generate_dataset.py i 10000 3 product
-python3 generate_dataset.py i 10000 3 customer
-python3 generate_dataset.py i 20000 3 product
-python3 generate_dataset.py i 20000 3 customer
-python3 generate_dataset.py i 50000 3 product
-python3 generate_dataset.py i 50000 3 customer
-python3 generate_dataset.py i 100000 3 product
-python3 generate_dataset.py i 100000 3 customer
-python3 generate_dataset.py i 200000 3 product
-python3 generate_dataset.py i 200000 3 customer
+# # skenario 1
+# python3 generate_dataset.py i 10000 3 product
+# python3 generate_dataset.py i 10000 3 customer
+# python3 generate_dataset.py i 20000 3 product
+# python3 generate_dataset.py i 20000 3 customer
+# python3 generate_dataset.py i 50000 3 product
+# python3 generate_dataset.py i 50000 3 customer
+# python3 generate_dataset.py i 100000 3 product
+# python3 generate_dataset.py i 100000 3 customer
+# python3 generate_dataset.py i 200000 3 product
+# python3 generate_dataset.py i 200000 3 customer
 
-# skenario 2
-python3 generate_dataset.py i 10000 4 product
-python3 generate_dataset.py i 10000 4 customer
-python3 generate_dataset.py i 10000 5 product
-python3 generate_dataset.py i 10000 5 customer
-python3 generate_dataset.py i 10000 6 product
-python3 generate_dataset.py i 10000 6 customer
-python3 generate_dataset.py i 10000 7 product
-python3 generate_dataset.py i 10000 7 customer
+# # skenario 2
+# python3 generate_dataset.py i 10000 4 product
+# python3 generate_dataset.py i 10000 4 customer
+# python3 generate_dataset.py i 10000 5 product
+# python3 generate_dataset.py i 10000 5 customer
+# python3 generate_dataset.py i 10000 6 product
+# python3 generate_dataset.py i 10000 6 customer
+# python3 generate_dataset.py i 10000 7 product
+# python3 generate_dataset.py i 10000 7 customer
 
-# coba doang
-python3 generate_dataset.py i 500 2 product
-python3 generate_dataset.py i 500 2 customer
-python3 generate_dataset.py i 1000 2 product
-python3 generate_dataset.py i 1000 2 customer
-python3 generate_dataset.py i 2000 2 product
-python3 generate_dataset.py i 2000 2 customer
+# # coba doang
+# python3 generate_dataset.py i 500 2 product
+# python3 generate_dataset.py i 500 2 customer
+# python3 generate_dataset.py i 1000 2 product
+# python3 generate_dataset.py i 1000 2 customer
+# python3 generate_dataset.py i 2000 2 product
+# python3 generate_dataset.py i 2000 2 customer
+
+python3 generate_dataset.py i 2000 3 product
+python3 generate_dataset.py i 2000 3 customer
+python3 generate_dataset.py i 5000 3 product
+python3 generate_dataset.py i 5000 3 customer
